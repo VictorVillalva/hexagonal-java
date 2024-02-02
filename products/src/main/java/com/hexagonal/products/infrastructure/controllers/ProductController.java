c2b5d9fdb88f2a684b16f58e4c667cdbab5d3069
Reus <victor.villalva2003@hotmail.com>
2024-02-02 00:43:00 -0600

    No soy backend x2

--- products/src/main/java/com/hexagonal/products/infrastructure/controllers/ProductController.java
+++ products/src/main/java/com/hexagonal/products/infrastructure/controllers/ProductController.java
@@ -1,5 +1,70 @@
 package com.hexagonal.products.infrastructure.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hexagonal.products.application.services.ProductService;
+import com.hexagonal.products.domain.models.AdditionalProductInfo;
+import com.hexagonal.products.domain.models.Product;
+
+@RestController
+@RequestMapping("/api/product/")
 public class ProductController {
-    
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long productId) {
+        return productService.getProductById(productId)
+                .map(task -> new ResponseEntity<>(task, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long productId, @RequestBody Product updatedProduct) {
+        return productService.updateProduct(productId, updatedProduct)
+                .map(task -> new ResponseEntity<>(task, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable Long productId) {
+        if (productService.deleteProduct(productId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{productId}/additional-info")
+    public ResponseEntity<AdditionalProductInfo> getAdditionalProductInfo(@PathVariable Long productId) {
+        AdditionalProductInfo additionalProductInfo = productService.getAdditionalProductInfo(productId);
+        return new ResponseEntity<>(additionalProductInfo, HttpStatus.OK);
+    }
 }
